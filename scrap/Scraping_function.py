f00cd8fafbe2cc88dab49888e01b9a69d989c18f
Rhamdan Syahrul Mubarak <141615487+rhamdansyahrulm@users.noreply.github.com>
2023-11-06 00:09:37 +0700

    Add files via upload

---
+++ scrap/Scraping_function.py
@@ -0,0 +1,89 @@
+from google.cloud import bigquery
+from google.oauth2 import service_account
+from scraping_review import get_reviews
+
+# Initialize BigQuery client
+credentials = service_account.Credentials.from_service_account_file("voltaic-reducer-399714-87eda49329ec.json")
+project_id = "voltaic-reducer-399714"
+client = bigquery.Client(credentials=credentials, project=project_id)
+
+# Retrieve product data
+table_id_product = "paragon.products_list"
+table_id_reviews = "paragon.reviews"
+query_product = f"""
+   SELECT *
+   FROM {table_id_product}
+   ORDER BY brand_id
+   """
+product_list_df = client.query(query_product).to_dataframe()
+
+# Retrieve unique brand data
+unique_brand = product_list_df[['brand_id', 'brand_name']].drop_duplicates().reset_index(drop=True)
+
+# Function to select items
+def select_items(prompt, column_name, df, selected_brand_list = []):
+    if column_name == "brand_id" :
+        print("=" * 75)
+        print(("\t" * 6) + "LIST OF PARAGON BRAND NAMES")
+        print("=" * 75)
+        for i, row in unique_brand.iterrows():
+            print(f"{row['brand_id']}. {row['brand_name']}")
+        print("=" * 75)
+    elif column_name == "product_id":
+        for i, row_1 in unique_brand.iterrows():
+            if str(row_1['brand_id']) in selected_brand_list:
+                print("=" * 75)
+                print(("\t" * 6) + f"LIST OF PARAGON PRODUCT NAMES ({row_1['brand_name']})")
+                print("=" * 75)
+                for i, row_2 in product_list_df.iterrows():
+                    if row_1["brand_id"] == row_2["brand_id"]:
+                        print(f"{row_2['product_id']}. {row_2['product_name']}")
+                print("=" * 75)
+    
+    selected_items = []
+    selected_names = []
+    choose = True
+    while choose:
+        selected_item = input(prompt)
+        selected_items.append(selected_item)
+        selected_names.extend(product_list_df.loc[product_list_df['product_id'] == int(selected_item), ["brand_name", "product_name", "product_shade", "product_url"]].values.tolist())
+        choose = input("Do you want to add another item? [Y/N]: ").strip().lower() == "y"
+    
+    print("=" * 75)
+    print(("\t" * 6) + f"YOUR SELECTED {column_name.upper()}")
+    print("=" * 75)
+    
+    for i, row in df.iterrows():
+        if str(row[column_name]) in selected_items:
+            print(f"{row[column_name]}. {row[column_name.replace('id', 'name')]}")
+    
+    print("=" * 75)
+    return selected_items, selected_names
+
+def time_interval():
+    interval_list = ["today", "1 Week Ago", "1 Month Ago", "1 Year Ago", "All"]
+    print("=" * 75)
+    print(("\t" * 6) + "TIME INTERVAL")
+    print("=" * 75)
+    for i in range(len(interval_list)):
+        print(f"{i+1}. {interval_list[i]}")
+    print("=" * 75)
+    
+    selected_interval = input("What time interval you want to take ? : ")
+    return interval_list[int(selected_interval)-1]
+    
+
+# Select brands
+brand, _ = select_items("Select the brand you want to search for: ", "brand_id", unique_brand)
+
+# Select products
+product_id, product_info = select_items("Select the Product you want to search for: ", "product_id", product_list_df, brand)
+
+interval = time_interval()
+    
+for info in product_info:
+    get_reviews(info, project_id, table_id_reviews, interval)
+    
+    
+    
+    
---
+++ scrap/scraping_review.py
@@ -0,0 +1,114 @@
+import time
+from datetime import datetime, timedelta
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+
+def get_reviews(product_info, project_id, table_id_reviews, interval):
+    brand_name, product_name, product_shade, url = product_info
+    
+    driver = webdriver.Chrome()
+    driver.get(url)
+    
+    next_page_available = True
+    continue_scrap = True
+    while next_page_available and continue_scrap:
+        page_review = []
+        time.sleep(1)
+        
+        for scroll in range(25):
+            driver.execute_script("window.scrollBy(0, 250)")
+            time.sleep(0.2)
+        
+        driver.execute_script("window.scrollBy(0, 50)")
+        time.sleep(0.2)
+        
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        
+        column_format = {
+            'age' : ['p', 'profile-age'],
+            'description' : ['p', 'profile-description'],
+            'date' : ['p', 'review-date'],
+            'review' : ['p', 'text-content'],
+            'rating' : ['span', 'cardrv-starlist'],
+            'recommend' : ['p', 'recommend'],
+            'usage_period' : ['div', 'information-wrapper'],
+            'purchase_point' : ['div', 'information-wrapper']
+        }
+        
+        for item in soup.findAll('div', class_='jsx-2016320139 jsx-2462230538 item'):
+            item_info = [brand_name, product_name, product_shade]
+            
+            for key, value in column_format.items():
+                element = item.find(value[0], class_=value[1])
+                
+                if key == "review":
+                    span_element = element.find('span') if element else None
+                    key_value = span_element.text if span_element is not None else ""
+                elif key == "usage_period":
+                    b_tags = element.find_all('b')
+                    key_value = b_tags[0].text if len(b_tags) > 0 else ""
+                elif key == "rating":
+                    score = element.find_all('i', class_="icon-ic_big_star_full margin-right")
+                    key_value = len(score)
+                elif key == "recommend":
+                    user_recomend = element.find('b').text if element is not None else ""
+                    key_value = 0 if "doesn't recommend" in user_recomend else None if user_recomend == "" else 1
+                elif key == "purchase_point":
+                    b_tags = element.find_all('b')
+                    key_value = b_tags[1].text if len(b_tags) > 1 else ""
+                elif key == "date":
+                    date_value = element.text if element is not None else ""
+                    if "hours ago" in date_value:
+                        key_value = (datetime.now() - timedelta(days=3)).strftime('%d %b %Y')
+                    elif "days ago" in date_value:
+                        days_ago = int(date_value.split()[0])
+                        key_value = (datetime.now() - timedelta(days=days_ago)).strftime('%d %b %Y')
+                    else :
+                        key_value = datetime.strptime(date_value, '%d %b %Y').strftime('%d %b %Y')
+                else :
+                    key_value = element.text if element is not None else ""
+                    
+                item_info.append(key_value)
+        
+            page_review.append(item_info)
+        
+        column_names = ["brand_name", "product_name", "product_shade", "age", "description", "date", "review", "rating", "recommend", "usage_period", "purchase_point"]
+        review_df = pd.DataFrame(page_review, columns=column_names)
+        
+        if interval == "today":
+            fix_df = review_df[review_df["date"] == datetime.now().strftime('%d %b %Y')]
+        elif interval == "1 Week Ago":
+            date_range = (datetime.now() - timedelta(days=6)).strftime('%d %b %Y')
+            fix_df = review_df[review_df["date"] >= date_range]
+        elif interval == "1 Month Ago":
+            date_range = (datetime.now() - timedelta(days=30)).strftime('%d %b %Y')
+            fix_df = review_df[review_df["date"] >= date_range]
+        elif interval == "1 Year Ago":
+            date_range = (datetime.now() - timedelta(days=365)).strftime('%d %b %Y')
+            fix_df = review_df[review_df["date"] >= date_range]
+        elif interval == "All":
+            fix_df = review_df
+        
+        fix_df.sort_values(by=["date"], ascending=False).to_gbq(table_id_reviews, project_id=project_id, if_exists="replace")
+                        
+        time.sleep(0.5)
+    
+        try:
+            next_button = driver.find_element(By.CSS_SELECTOR, '#id_next_page.paging-prev-text-active')
+            next_button.click()
+            time.sleep(5)
+        except NoSuchElementException:
+            print("This is the last page !")
+            next_page_available = False
+            driver.close()
+        
+        if len(fix_df) < 10 :
+            continue_scrap = False
+            driver.close()
+        else : True
+    
+    return review_df, fix_df, page_review, item_info
